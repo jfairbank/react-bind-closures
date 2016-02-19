7df14cfdee08560dc07f24113a92726ddd74eeec
Jeremy Fairbank <elpapapollo@gmail.com>
2016-02-18 19:06:26 -0500

    Change API

--- src/index.js
+++ src/index.js
@@ -5,43 +5,33 @@ export default function bindClosures(closuresMap) {
     const componentName = Component.displayName || Component.name || 'Component';
     const closureNames = Object.keys(closuresMap);
 
-    const methods = closureNames.reduce((memo, key) => {
-      const argsProducer = closuresMap[key];
+    const spec = closureNames.reduce((memo, closureName) => {
+      const injectedClosure = closuresMap[closureName];
 
-      memo[key] = function() {
-        const originalPropFunction = this.props[key];
-        const args = argsProducer(this.props);
-
-        if (!args) {
-          return originalPropFunction();
-        }
-
-        switch (args.length) {
-          case 0: return originalPropFunction();
-          case 1: return originalPropFunction(args[0]);
-          case 2: return originalPropFunction(args[0], args[1]);
-          case 3: return originalPropFunction(args[0], args[1], args[2]);
-          case 4: return originalPropFunction(args[0], args[1], args[2], args[3]);
-          default: return originalPropFunction.apply(null, args);
-        }
+      memo[closureName] = function boundClosure(...args) {
+        return injectedClosure(this.props, ...args);
       };
 
       return memo;
     }, {});
 
-    methods.render = function render() {
-      const closures = closureNames.reduce((memo, key) => {
-        memo[key] = this[key];
+    spec.componentWillMount = function componentWillMount() {
+      this.__closures = closureNames.reduce((memo, closureName) => {
+        memo[closureName] = this[closureName];
         return memo;
       }, {});
-
-      return Component({
-        ...this.props,
-        ...closures
-      });
     };
 
-    const Wrapper = React.createClass(methods);
+    spec.render = function render() {
+      const newProps = {
+        ...this.props,
+        ...this.__closures,
+      };
+
+      return Component(newProps);
+    };
+
+    const Wrapper = React.createClass(spec);
     Wrapper.displayName = `ClosureWrapper(${componentName})`;
 
     return Wrapper;
